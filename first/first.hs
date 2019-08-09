887e558c22ad58ca2c91a3e7020db83c44b98e27
Daniel Gomes <daniel0x7cc@gmail.com>
2019-08-08 23:14:36 -0300

    created baby functions

--- first/first.hs
+++ first/first.hs
@@ -4,7 +4,17 @@ main = do
 test = do
     print "Test function"
 
-doubleMe x = x + x 
+doubleMe x = x + x
+
+sumUs x y = x + y
+
+subtractUs x y = x - y
+
+multiplyUs x y = x * y
+
+divideUs x y = x / y
+
+doubleEvenNumber x = if x `mod` 2 == 0 then x * 2 else x * 3
 
 bum = do
     let lostNumbers = [4,8,15,16,23,42]
